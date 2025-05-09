ba530c5f51da0dc3de1b6df2d0d4a7da9117b5ef
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-05-09 09:43:16 +0700

    Add WebFormTC test case

---
+++ src/test/java/testCase/WebFormTC.java
@@ -0,0 +1,27 @@
+package testCase;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+import pageEvents.SubmittedPageEvents;
+import pageEvents.WebFormPageEvents;
+
+public class WebFormTC extends BaseTest {
+    WebFormPageEvents webFormPageEvents = new WebFormPageEvents();
+    SubmittedPageEvents submittedPageEvents = new SubmittedPageEvents();
+
+    @Test
+    public void testWebForm() {
+        webFormPageEvents.verifyPageTitle();
+        webFormPageEvents.verifyHeading();
+        webFormPageEvents.inputText();
+        webFormPageEvents.inputPassword();
+        webFormPageEvents.inputTextArea();
+        webFormPageEvents.verifyDisabledInput();
+        webFormPageEvents.verifyReadOnlyInput();
+        webFormPageEvents.clickSubmitButton();
+
+        submittedPageEvents.verifyPageTitle();
+        submittedPageEvents.verifyHeading();
+        submittedPageEvents.verifyMessageText();
+    }
+}

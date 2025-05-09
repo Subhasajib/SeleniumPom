a104f2d70434a32bfe4e83a72ca1c679ddfd7384
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2025-05-09 07:48:27 +0700

    Add WebFormPageElements

---
+++ src/main/java/pageObjects/WebFormPageElements.java
@@ -0,0 +1,12 @@
+package pageObjects;
+
+public interface WebFormPageElements {
+    String headingXPath = "/html/body/main/div/div/div/h1";
+    String textInputId = "my-text-id";
+    String passwordInputName = "my-password";
+    String textAreaInputName = "my-textarea";
+    String disableInputName = "my-disabled";
+    String readOnlyInputName = "my-readonly";
+
+    String submitButtonXPath = "/html/body/main/div/form/div/div[2]/button";
+}
